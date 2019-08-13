c2a2e211ce3027ce94a4dd74c9ce0886f4b29b71
rener172846 <rener172846@hotmail.com>
2019-08-13 19:16:46 +0800

    add file uploader

---
+++ src/middleware/uploader/index.js
@@ -0,0 +1,3 @@
+import s3FileUploader from './s3FileUploader';
+
+export default s3FileUploader;
---
+++ src/middleware/uploader/s3FileUploader.js
@@ -0,0 +1,65 @@
+import AWS from 'aws-sdk';
+import axios from 'axios';
+import Url from 'url-parse';
+import moment from 'moment';
+import jwt from 'jsonwebtoken';
+import uuid from 'uuid/v4';
+
+import config from '../../config';
+
+
+let s3: AWS.S3 = new AWS.S3({
+  accessKeyId: config.aws_accesskey,
+  secretAccessKey: config.aws_secretkey,
+});
+
+
+class S3FileUploader {
+  async uploadImageFromUrl(url: string) {
+    try
+    {
+      if(!url || typeof url != "string") throw new Error("Missing parameter \'url\'.");
+      
+      const parsed = new Url(url);
+      let extension = "png", query = {};
+      if (parsed && parsed.query) {
+        query = qs.parse(parsed.query)
+      }
+
+      let fileName: string = parsed.pathname.substring(parsed.pathname.lastIndexOf("/")+1);
+
+      if (fileName.lastIndexOf(".") > -1) {
+        extension = fileName.split('.')[1];
+      }
+
+      const rand = uuid();
+      const token = jwt.sign(rand, config.secret_key);
+      const fileKey = `${rand}.${extension}`;
+      const fileUrl = `${config.server_root_url}public/media?name=${fileKey}&sk=${token}&da=${moment().format()}`;
+      console.log('fileUrl', fileUrl)
+
+      let response = await axios.get(url).then( async result => {
+        return await s3.putObject({
+          ACL: "public-read",
+          Bucket: config.aws_s3_bucket,
+          Body : result.data,
+          Key : fileKey,
+          ContentType: `image/png`,
+        }).promise();
+      })
+      .catch( err => {
+        throw err;
+      });
+        console.log('response of image', response)
+      
+      return fileUrl;
+    } catch(ex){
+      console.log("Error on utility.S3FileUpload.uploadImageFromUrl: " + ex);
+      throw ex;
+    }
+  };
+}
+
+let s3FileUploader = new S3FileUploader();
+
+export default s3FileUploader;
