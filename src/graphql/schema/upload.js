8815b35c25c7fe7dfaedab730b2d832bc7a18b4c
rener172846 <rener172846@hotmail.com>
2019-10-01 03:39:31 +0800

    add uploading video API with generating thumbnail

--- src/graphql/schema/upload.js
+++ src/graphql/schema/upload.js
@@ -5,6 +5,7 @@ import { gql } from 'apollo-server-express';
 export default gql`
   type File {
     url: String!
+    thumbUrl: String,
     filename: String!
     mimetype: String!
     encoding: String!
